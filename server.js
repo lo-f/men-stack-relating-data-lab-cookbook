cdd1c0eb6b842c4b12ba309374e569f9c3fec190
lo-franky <lo.frankyl@gmail.com>
2024-07-17 16:58:52 -0500

    making progress

--- server.js
+++ server.js
@@ -8,8 +8,14 @@ const morgan = require('morgan');
 const session = require('express-session');
 
 const authController = require('./controllers/auth.js');
+const foodsController = require('./controllers/foods.js');
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
 
 const port = process.env.PORT ? process.env.PORT : '3000';
+
+/* ------------------------------- CONTROLLERS ------------------------------ */
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -17,6 +23,7 @@ mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
+/* ------------------------------- MIDDLEWARE ------------------------------- */
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 // app.use(morgan('dev'));
@@ -28,6 +35,7 @@ app.use(
   })
 );
 
+/* --------------------------------- ROUTES --------------------------------- */
 app.get('/', (req, res) => {
   res.render('index.ejs', {
     user: req.session.user,
@@ -42,7 +50,11 @@ app.get('/vip-lounge', (req, res) => {
   }
 });
 
+app.use(passUserToView);
 app.use('/auth', authController);
+app.use(isSignedIn);
+app.use('/users/:userId/foods',foodsController);
+
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
