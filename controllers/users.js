258d60baff6bff7aea7b98b220562e2a534160c9
lo-franky <lo.frankyl@gmail.com>
2024-07-18 10:20:51 -0500

    got user.js and user/index.ejs working

---
+++ controllers/users.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user.js');
+
+/* --------------------------------- LANDING -------------------------------- */
+
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.render('users/index.ejs', { users })
+      } catch (error) {
+        console.error(error);
+        res.redirect('/');
+      }
+    });
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const session = require('express-session');
 
 const authController = require('./controllers/auth.js');
 const foodsController = require('./controllers/foods.js');
+const usersController = require('./controllers/users.js');
 const isSignedIn = require('./middleware/is-signed-in.js');
 const passUserToView = require('./middleware/pass-user-to-view.js');
 
@@ -53,8 +54,8 @@ app.get('/vip-lounge', (req, res) => {
 app.use(passUserToView);
 app.use('/auth', authController);
 app.use(isSignedIn);
-app.use('/users/:userId/foods',foodsController);
-
+app.use('/users/:userId/foods', foodsController);
+app.use('/users/:userId/community', usersController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
